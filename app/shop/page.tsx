024f04f8dd86f3e92e6835369157c2415b128b19
Nikita Popov <losnikitos@gmail.com>
2025-08-17 13:08:16 +0100

    asdf

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -98,12 +98,12 @@ export default function ShopPage() {
             </div>
             
             {/* Category Filter */}
-            <div className="relative">
+            <div className="relative hidden md:block">
               <Filter className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
               <select
                 value={selectedCategory}
                 onChange={(e) => setSelectedCategory(e.target.value)}
-                className="pl-10 pr-8 py-2 border border-gray-300 dark:border-gray-600 rounded-lg bg-white dark:bg-gray-700 text-gray-900 dark:text-white focus:ring-2 focus:ring-blue-500 focus:border-transparent appearance-none cursor-pointer"
+                className="pl-10 pr-8 w-full py-2 border border-gray-300 dark:border-gray-600 rounded-lg bg-white dark:bg-gray-700 text-gray-900 dark:text-white focus:ring-2 focus:ring-blue-500 focus:border-transparent appearance-none cursor-pointer"
               >
                 <option value="All">All Categories</option>
                 {categories.map(category => (
